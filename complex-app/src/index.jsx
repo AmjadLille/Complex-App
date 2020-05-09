0a9025322fcd25286a4915f1fe98a18a3cfd8b1b
Amjad <sammanamjad86@gmail.com>
2020-05-09 19:39:35 +0200

    add about and terms , router

--- complex-app/src/index.jsx
+++ complex-app/src/index.jsx
@@ -5,18 +5,31 @@ import "./main.css";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 import HomeGuest from "./components/HomeGuest";
+import About from "./components/About";
+import Terms from "./components/Terms";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
 
-function ExampleComponent() {
+function Main() {
   return (
-    <>
+    <BrowserRouter>
       <Header />
-      <HomeGuest />
+      <Switch>
+        <Route path="/" exact>
+          <HomeGuest />
+        </Route>
+        <Route path="/about-us">
+          <About />
+        </Route>
+        <Route path="/terms">
+          <Terms />
+        </Route>
+      </Switch>
       <Footer />
-    </>
+    </BrowserRouter>
   );
 }
 
-ReactDOM.render(<ExampleComponent />, document.getElementById("app"));
+ReactDOM.render(<Main />, document.getElementById("app"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
